7ed0c14673dac43a7ed871a2533dab69bbd6e7f6
fishru <zgcjack@gmail.com>
2023-05-17 13:59:56 +0800

    Add files via upload

---
+++ Scripts/TCPSocket.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+using System.Threading;
+
+public class TCPSocket : MonoBehaviour
+{
+    [SerializeField] Test2Controller controller;
+    //[SerializeField] Transform target;
+    // HTTP References:
+    // https://gist.github.com/sdumetz/ca490544f200c7b2a92f
+    // https://learn.microsoft.com/en-us/dotnet/api/system.net.httplistener
+    // https://learn.microsoft.com/en-us/dotnet/api/system.net.sockets.tcplistener
+
+    //Json References:
+    //https://docs.unity3d.com/ScriptReference/JsonUtility.FromJson.html
+
+    Thread httpListenerThread;
+    int actionID;
+    float actionValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string[] prefix = new string[] { "http://127.0.0.1:8080/" };
+        httpListenerThread = new Thread(new ThreadStart(Listener));
+        httpListenerThread.IsBackground = true;
+        httpListenerThread.Start();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+        httpListenerThread.Abort();
+        Debug.Log("http stop!");
+    }
+
+    void Listener()
+    {
+        HttpListener listener = new HttpListener();
+        try
+        {
+            string[] prefixes = new string[] { "http://127.0.0.1:8080/" };
+            // URI prefixes are required,
+            // for example "http://contoso.com:8080/index/".
+            if (prefixes == null || prefixes.Length == 0)
+                throw new ArgumentException("prefixes");
+
+            // Create a listener.
+            
+            // Add the prefixes.
+            foreach (string s in prefixes)
+            {
+                listener.Prefixes.Add(s);
+            }
+            listener.Start();
+            Debug.Log("Listening...");
+            while (true)
+            {
+                string text = "test";
+                // Note: The GetContext method blocks while waiting for a request.
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                // Obtain a response object.
+                HttpListenerResponse response = context.Response;
+
+                using var reader = new StreamReader(request.InputStream);
+                text = reader.ReadToEnd();
+                JsonData data = JsonUtility.FromJson<JsonData>(text);
+                //Debug.Log("Action ID: " + data.id + ", Action Value: " + data.value );
+                actionID = data.id;
+                actionValue = data.value;
+                controller.Action(data.id, data.value);
+
+
+                // Construct a response.
+                string responseString = "<HTML><BODY>Unity 3D Hello world!</BODY></HTML>";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                // Get a response stream and write the response to it.
+                response.ContentLength64 = buffer.Length;
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                // You must close the output stream.
+                output.Close();
+                
+
+            }
+        }
+        catch (SocketException socketException)
+        {
+            Debug.Log("SocketException " + socketException.ToString());
+        }
+        finally //要加此區塊才能在遊戲結束時正常結束此thread
+        {
+            listener.Stop();
+        }
+    }
+
+
+}
---
+++ Scripts/Test2Controller.cs
@@ -0,0 +1,71 @@
+using StarterAssets;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Test2Controller : MonoBehaviour
+{
+    [SerializeField] StarterAssetsInputs inputs;
+    [SerializeField] Vector2 direction;
+    [SerializeField] float jumpState;
+    [SerializeField] float sprintState;
+    int hMove = 100;
+    int vMove = 101;
+    int jump = 102;
+    int sprint = 103;
+    float hInput;
+    float vInput;
+    float waitTime = 0.5f;
+    float timer = 0.0f;
+    Dictionary<int, Action<float>> actionList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        actionList = new()
+        {
+            { hMove, x => { timer = 0.0f; hInput = x; vInput = 0; } },
+            { vMove, x => { timer = 0.0f; vInput = x; hInput = 0; } },
+            { jump, x => jumpState = x },
+            { sprint, x => sprintState = x }
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= waitTime)
+        {
+            hInput = 0;
+            vInput = 0;
+            timer = 0.0f;
+        }
+
+        direction.x = hInput;
+        direction.y = vInput;
+        inputs.MoveInput(direction);
+        jumpCheck();
+        sprintCheck();
+    }
+
+    private void jumpCheck()
+    {
+        if (jumpState > 0)
+        {
+            inputs.JumpInput(Convert.ToBoolean(jumpState));
+        }
+        jumpState = 0;
+    }
+
+    private void sprintCheck()
+    {
+        inputs.SprintInput(Convert.ToBoolean(sprintState));
+    }
+
+    public void Action(int actionID, float actionValue)
+    {
+        Debug.Log("actionID: " + actionID + ", actionValue: " + actionValue);
+        actionList[actionID].DynamicInvoke(actionValue);
+    }
+}
